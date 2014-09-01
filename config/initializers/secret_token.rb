d25fad88dbd525d3f0dad0ac9b61a5d10cdbf668
Nick Meehan <nnmeehan@gmail.com>
2014-09-01 15:28:38 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TransitYo::Application.config.secret_key_base = 'b7eb8a515253849f41651fa55bd829776f9dc8cffa8d3ccecdf24c864fc2b066e01843d851de2294faa5337f47cc6ac3f80b6faf50d4240624ac3c0e8a2fba26'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TransitYo::Application.config.session_store :cookie_store, key: '_transit-yo_session'
