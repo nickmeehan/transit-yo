ead005185e1d9f794b661c389f7694df5d345933
Nick Meehan <nnmeehan@gmail.com>
2014-09-03 13:18:22 -0600

    Creating or logging in a user via YoAuth
    Adding code to create or log user in.
    
    YoAuth integration is now set.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,13 +13,24 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.new(@yo_user_details)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      
+    end
   end
 
   def auth
-    yo_user_details = YoAuth.get_yo_username(params[:yoauth_token])
-    @user = User.find_by(yo_username: yo_user_details[:yo_username])
-    render :index
+    @yo_user_details = YoAuth.get_yo_username(params["yoauth_token"])
+    @user = User.find_by(yo_username: @yo_user_details[:yo_username])
+    if @user != nil
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      create
+    end
   end
 
 end
--- app/models/yo_auth.rb
+++ app/models/yo_auth.rb
@@ -8,7 +8,7 @@ class YoAuth
   def self.handle_getting_of_yo_username_response(response)
     if response.code == 200 || response.code == 201
       parsed_response = ActiveSupport::JSON.decode(response.body)
-      return { yo_user_id: parsed_response[:user][:id], yo_username: parsed_response[:user][:yo_username] }
+      return { yo_user_id: parsed_response["user"]["id"], yo_username: parsed_response["user"]["yo_username"] }
     else
       return "Error"
     end
